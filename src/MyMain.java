82c8bc1779e1b1c71b05079b4b8bbf522890d7d1
Faschal <faschal21@gmail.com>
2021-01-11 14:50:54 +0700

    Tutorial variables

--- src/MyMain.java
+++ src/MyMain.java
@@ -2,8 +2,25 @@
 public class MyMain {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
-
+		int x = 122;
+		double y = 4.5232;
+		boolean z = true;
+		char symbol = '#';
+		String name = "Alex";
+		String a = "Blue";
+		String b = "Red";
+		String temp;
+		temp = a;
+		a = b;
+		b = temp;
+		
+		System.out.println("Number : " + x);
+		System.out.println("Float : " + y);
+		System.out.println("Boolean :" + z);
+		System.out.println("char : " + symbol);
+		System.out.println("string : " + name);
+		System.out.println("a :" + a);
+		System.out.println("b :" + b);		
 	}
 
 }
