f7652da26e5fa346641819c8624d726e052d11eb
Faschal <faschal21@gmail.com>
2021-01-12 10:29:16 +0700

    Tutorial logical operator

---
+++ src/IFStatement.java
@@ -0,0 +1,53 @@
+
+public class IFStatement {
+
+	public static void main(String[] args) {		
+//		int age = 12;
+//		
+//		if (age > 18)
+//		{
+//			System.out.println("Age is greater than 18");
+//		}
+//		else if (age == 18)
+//		{
+//			System.out.println("Age is 18");
+//		}
+//		else
+//		{
+//			System.out.println("Age is lower than 18");
+//		}
+//		
+//		switch(age) 
+//		{
+//			case 18:
+//				System.out.println("Your are 18 !");
+//				break;
+//			case 19:
+//				System.out.println("Your are 19 !");
+//				break;
+//			case 17:
+//				System.out.println("Your are 17 !");
+//				break;
+//			default:
+//				System.out.println("Default statement");
+//				break;
+//		}
+		
+		int temp = 1;
+		
+		if (temp > 30)
+		{
+			System.out.println("It is hot");
+		}
+		else if (temp>=20 && temp<=30)
+		{
+			System.out.println("It is warm");
+		}
+		else
+		{
+			System.out.println("It is cold");
+		}
+		
+	}
+
+}
