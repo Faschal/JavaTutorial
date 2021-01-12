d8f9315d3c4348cd633b8c7ccc763275ceb2f5a6
Faschal <faschal21@gmail.com>
2021-01-12 10:10:39 +0700

    Tutorial Math

---
+++ src/MathTest.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class MathTest {
+
+	public static void main(String[] args) {
+		Random random = new Random();
+		
+		int x = random.nextInt(6)+1;
+		double y = random.nextDouble();
+		boolean z= random.nextBoolean();
+//		double x;
+//		double y;
+//		double z;
+//		
+//		Scanner scanner = new Scanner(System.in);
+//		
+//		System.out.println("Enter x : ");
+//		x = scanner.nextDouble();
+//		System.out.println("Enter y : ");
+//		y = scanner.nextDouble();
+//		
+//		z = Math.sqrt((x * x) + (y * y));
+//		System.out.println("hypotenuse " + z);
+//		
+//		scanner.close();
+		
+		System.out.println(z);
+		
+	}
+
+}
