ff9a144bc42df3eb50c79b0a7857f3ade654411f
Faschal <faschal21@gmail.com>
2021-01-13 13:00:29 +0700

    Tutorial access modifier

---
+++ src/package1/A.java
@@ -0,0 +1,12 @@
+package package1;
+import package2.*;
+
+public class A {
+
+	protected String protectedMsg = "This is a protected message";
+	
+	public static void main(String[] args) {
+		B b = new B();		
+	}
+
+}
---
+++ src/package2/C.java
@@ -0,0 +1,10 @@
+package package2;
+import package1.*;
+
+public class C {
+	
+	public String publicMsg = "This is a public message";
+	String defaultMsg = "This is a default message";
+	protected String protectedMsg = "This is a protected message";
+	private String privateMsg = "This is a private message";
+}
