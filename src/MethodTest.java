9f956d44acb84f11d99c230c8714d300a10b3e07
Faschal <faschal21@gmail.com>
2021-01-13 10:04:48 +0700

    Tutorial method

---
+++ src/MethodTest.java
@@ -0,0 +1,13 @@
+
+public class MethodTest {
+
+	public static void main(String[] args) {		
+		System.out.println(sum(12, 54));		
+	}
+	
+	static int sum(int x, int y)
+	{ 
+		return x + y;
+	}
+	
+}
