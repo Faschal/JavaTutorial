83ac5dd5e6972de4259c211266a0e24fe7f4781b
Faschal <faschal21@gmail.com>
2021-01-13 10:23:54 +0700

    Tutorial printf

---
+++ src/PrintfTest.java
@@ -0,0 +1,15 @@
+
+public class PrintfTest {
+
+	public static void main(String[] args) {
+		double myDouble = 100;
+		
+		System.out.printf("Hello %s \n", "Faschal");
+		System.out.printf("string %s \n", "Karel");
+		System.out.printf("int %d \n", 21);
+		System.out.printf("boolean %b \n", true);
+		System.out.printf("char %c \n", 'c');
+		System.out.printf("double %.2f \n", myDouble);
+	}
+
+}
