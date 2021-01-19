5e061076edea5902a9dee1ece903cec6316b2db9
Faschal <faschal21@gmail.com>
2021-01-19 12:09:58 +0700

    Tutorial Thread

---
+++ src/ThreadTest.java
@@ -0,0 +1,15 @@
+
+public class ThreadTest {
+
+	public static void main(String[] args) {
+		MyThread1 thread1 = new MyThread1();
+		
+		MyRunable runable1 = new MyRunable();
+		Thread thread2 = new Thread(runable1);
+		
+		thread1.start();	
+		thread2.start();
+		
+	}
+
+}
